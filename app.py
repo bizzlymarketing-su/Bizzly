ad219348ce6f1f61196ab181f4e411390770f17e
bizzlymarketing-su <bizzlymarketing@gmail.com>
2026-05-11 15:00:32 -0700

    increase product description length

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     business_id = db.Column(db.Integer, db.ForeignKey('business.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(200))
+    description = db.Column(db.Text)
     image = db.Column(db.String(200), default="images/default_product.png")
     clicks = db.Column(db.Integer, default=0)
 
